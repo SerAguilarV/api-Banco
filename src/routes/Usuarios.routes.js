da1e6dd8a5ae2464b922a2585e692e23ba16a1c6
unknown <ser.aguilarv@gmail.com>
2020-05-19 14:46:40 -0500

    [DELETE] Route /:idMongo/IDBanco

--- src/routes/Usuarios.routes.js
+++ src/routes/Usuarios.routes.js
@@ -2,10 +2,10 @@ const {Router} = require("express")
 const {AuthMiddleware, ParseMiddleware} = require("../middlewares")
 module.exports = function RouterUsuario({ControllerUsuario}){
     const router = Router()
-    router.get("/:idmongo/info", ControllerUsuario.get_info)
-    router.get("/:idmongo/IDBanco", ControllerUsuario.get_ID)
-    router.get("/allusers", ControllerUsuario.getAll)
-    router.get("/search", ControllerUsuario.search)
+    router.get("/:idmongo/info", [AuthMiddleware, ParseMiddleware] , ControllerUsuario.get_info)
+    // router.get("/:idmongo/IDBanco", [AuthMiddleware, ParseMiddleware] ,ControllerUsuario.get_ID)
+    router.get("/allusers", [AuthMiddleware, ParseMiddleware] ,ControllerUsuario.getAll)
+    router.get("/search", [AuthMiddleware, ParseMiddleware] , ControllerUsuario.search)
     router.patch("/:idMongo", [AuthMiddleware, ParseMiddleware] ,ControllerUsuario.update)
     return router
 }
